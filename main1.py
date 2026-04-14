2db270f89c81c0e3408f4fd4eb1b657e15d505f2
lliancastro <lianc4793@gmail.com>
2026-04-14 08:32:29 +0800

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,69 @@
+#CLASSMATE 1
+class Classmate: 
+    def __init__(self,name,section,favorite_subject):
+        self.name=name
+        self.section=section
+        self.favorite_subject=favorite_subject
+
+Classmate1= Classmate("Arabella", "10E", "TLE")
+
+def introduce(self):
+    return "Hi! I am {Arabella from 10E, and my favorite subject is TLE!}."
+
+#CLASSMATE 2
+
+class Classmate: 
+    def __init__(self,name,section,favorite_subject):
+        self.name=name
+        self.section=section
+        self.favorite_subject=favorite_subject
+
+Classmate2= Classmate("Emille", "10E", "Math")
+
+def introduce(self):
+    return "Hi! I am {Emille from 10E, my favorite subject is Math.}."
+
+
+#CLASSMATE 3 
+
+class Classmate: 
+    def __init__(self,name,section,favorite_subject):
+        self.name=name
+        self.section=section
+        self.favorite_subject=favorite_subject
+
+Classmate3= Classmate("Rycob", "10E", "Science")
+
+def introduce(self):
+    return "Hi! I am {Rycob from 10E, and my favorite subject is Science. }."
+
+
+#CLASSMATE 4 
+
+class Classmate: 
+    def __init__(self,name,section,favorite_subject):
+        self.name=name
+        self.section=section
+        self.favorite_subject=favorite_subject
+
+Classmate4= Classmate("Gianna", "10E", "Music and Arts")
+
+def introduce(self):
+    return "Hi! I am {Gianna from 10E, my favorite subject is Music and Arts!}."
+
+#CLASSMATE 5 
+
+class Classmate: 
+    def __init__(self,name,section,favorite_subject):
+        self.name=name
+        self.section=section
+        self.favorite_subject=favorite_subject
+
+Classmate5= Classmate("Lian", "10E", "English")
+
+def introduce(self):
+    return "Hi! I am {Lian from 10E, and my favorite subject is English!}."
+
+
+
+
